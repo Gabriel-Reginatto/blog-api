c32b7ec59e6069ef422f748fcdcc6b7ab6b2d4b8
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-16 15:52:33 -0300

    refactor: rename repository and mapper to UserRepository and UserMapper

--- src/main/java/br/com/blog/api/repositories/UserRepository.java
+++ src/main/java/br/com/blog/api/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import br.com.blog.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/br/com/blog/api/services/UserService.java
+++ src/main/java/br/com/blog/api/services/UserService.java
@@ -1,6 +1,5 @@
 package br.com.blog.api.services;
 
-import br.com.blog.api.dto.user.UserCreateRequestDTO;
 import br.com.blog.api.dto.user.UserResponseDTO;
 import br.com.blog.api.entities.User;
 import br.com.blog.api.exception.ResourceNotFoundException;
@@ -14,37 +13,38 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-    private final UserRepository repository;
-    private final UserMapper mapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository repository, UserMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     public UserResponseDTO findById(Long id) {
 
         logger.info("Finding user with ID: {}", id);
 
-        User entity =  repository.findById(id)
+        User entity =  userRepository.findById(id)
                 .orElseThrow(
                         () -> new ResourceNotFoundException("user", id)
                 );
 
-        return mapper.toResponseDTO(entity);
+        return userMapper.toResponseDTO(entity);
     }
 
     public UserResponseDTO findByUsername(String username) {
 
         logger.info("Finding user by username: {}", username);
 
-        var entity = repository.findByUsername(username)
+        var entity = userRepository.findByUsername(username)
                 .orElseThrow(
                         () -> new ResourceNotFoundException("user", "username", username)
                 );
 
-        return mapper.toResponseDTO(entity);
+        return userMapper.toResponseDTO(entity);
     }
 
+    public UserResponseDTO
 
 }

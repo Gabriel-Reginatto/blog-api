74bd2b7018411f443f15e9ee2d05b4af40ea0355
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-16 20:41:08 -0300

    feat: add UserUpdateRequestDTO to User

--- src/main/java/br/com/blog/api/dto/user/UserUpdateRequestDTO.java
+++ src/main/java/br/com/blog/api/dto/user/UserUpdateRequestDTO.java
@@ -1,8 +1,23 @@
 package br.com.blog.api.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public record UserUpdateRequestDTO(
+
+        @Size(min = 3, max = 50, message = "username must be between 3 and 50 characters")
         String username,
+
+        @Email(message = "email must be valid")
         String email,
+
+        @Size(min = 2, max = 50, message = "first name must be between 2 and 50 characters")
+        String firstName,
+
+        @Size(min = 2, max = 100, message = "last name must be between 2 and 100 characters")
+        String lastName,
+
+        @Size(max = 500, message = "bio max 500 characters")
         String bio
-) {
-}
+
+) {}

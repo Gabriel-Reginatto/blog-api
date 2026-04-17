cc2afaeb3b1e5870190c134ecfb6f934ce9274a6
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-17 16:38:13 -0300

    feat: add createPost to Post

--- src/main/java/br/com/blog/api/repositories/UserRepository.java
+++ src/main/java/br/com/blog/api/repositories/UserRepository.java
@@ -13,7 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    Optional<User> findByEmail(String email);
-
     boolean existsByEmail(String email);
 }
---
+++ src/main/java/br/com/blog/api/services/PostService.java
@@ -0,0 +1,72 @@
+package br.com.blog.api.services;
+
+import br.com.blog.api.dto.post.PostCreateRequestDTO;
+import br.com.blog.api.dto.post.PostResponseDTO;
+import br.com.blog.api.entities.Category;
+import br.com.blog.api.entities.Post;
+import br.com.blog.api.entities.User;
+import br.com.blog.api.exception.ResourceNotFoundException;
+import br.com.blog.api.mapper.PostMapper;
+import br.com.blog.api.repositories.CategoryRepository;
+import br.com.blog.api.repositories.PostRepository;
+import br.com.blog.api.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final PostMapper postMapper;
+    private static final Logger logger = LoggerFactory.getLogger(PostService.class);
+
+    public PostService(PostRepository postRepository, UserRepository userRepository, CategoryRepository categoryRepository, PostMapper postMapper) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+        this.postMapper = postMapper;
+    }
+
+    public PostResponseDTO createPost(PostCreateRequestDTO request) {
+
+        logger.info("Creating post with author: {}", request.authorUsername());
+
+        var user = userRepository.findByUsername(request.authorUsername())
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("authorUsername")
+                );
+
+        var categories = categoryRepository.findAllById(request.categoryIds());
+
+        if (categories.size() != request.categoryIds().size()) {
+            Set<Long> foundIds = categories.stream()
+                    .map(Category::getId)
+                    .collect(Collectors.toSet());
+
+            List<Long> notFoundIds = request.categoryIds().stream()
+                    .filter(id -> !foundIds.contains(id))
+                    .toList();
+
+            throw new ResourceNotFoundException("Categories not found with IDs: " + notFoundIds);
+        }
+
+        Set<Category> cat = new HashSet<>(categories);
+
+        Post post = postMapper.toEntity(request);
+        post.setAuthor(user);
+        post.setCategories(cat);
+
+        Post savedPost = postRepository.save(post);
+
+        return postMapper.toResponseDTO(savedPost);
+    }
+
+}

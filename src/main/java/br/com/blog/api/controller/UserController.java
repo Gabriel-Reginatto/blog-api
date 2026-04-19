a3c6de8fdecdbd771c7add6330ce12065ffac268
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-19 11:24:41 -0300

    feat: add findById to UserController

--- src/main/java/br/com/blog/api/controller/UserController.java
+++ src/main/java/br/com/blog/api/controller/UserController.java
@@ -1,10 +1,29 @@
 package br.com.blog.api.controller;
 
+import br.com.blog.api.dto.user.UserResponseDTO;
+import br.com.blog.api.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+
+        return ResponseEntity.ok(user);
+    }
+
 }
+

9b28b9a3367f9399cfaccfc246f898b03525ee00
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-19 20:25:45 -0300

    feat: create PostController to Post

---
+++ src/main/java/br/com/blog/api/controller/PostController.java
@@ -0,0 +1,30 @@
+package br.com.blog.api.controller;
+
+import br.com.blog.api.dto.post.PostResponseDTO;
+import br.com.blog.api.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> findById(@PathVariable Long id) {
+        PostResponseDTO post = postService.findById(id);
+        return ResponseEntity.ok(post);
+    }
+
+
+
+
+}

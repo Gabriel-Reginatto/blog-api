463970e88b17c346089e3b93b75fa89c864fb0ba
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-19 11:21:24 -0300

    feat: create UserController to User

---
+++ src/main/java/br/com/blog/api/controller/UserController.java
@@ -0,0 +1,10 @@
+package br.com.blog.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users/v1")
+public class UserController {
+
+}

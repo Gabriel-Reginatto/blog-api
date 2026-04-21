499289183bc0ec61893c2fa5fb70a9d255e4d393
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-20 23:06:17 -0300

    feat: add findById method with HATEOAS

--- src/main/java/br/com/blog/api/controller/UserController.java
+++ src/main/java/br/com/blog/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.blog.api.controller;
 
+import br.com.blog.api.assembler.UserModelAssembler;
 import br.com.blog.api.dto.user.request.UserCreateRequestDTO;
 import br.com.blog.api.dto.user.response.UserResponseDTO;
 import br.com.blog.api.dto.user.request.UserUpdateRequestDTO;
@@ -7,7 +8,9 @@ import br.com.blog.api.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +23,26 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class UserController {
 
     private final UserService userService;
+    private final UserModelAssembler userAssembler;
+    private final PagedResourcesAssembler<UserResponseDTO> pagedAsembler;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserModelAssembler userAssembler, PagedResourcesAssembler<UserResponseDTO> pagedAssembler) {
         this.userService = userService;
+        this.userAssembler = userAssembler;
+        this.pagedAsembler = pagedAssembler;
     }
 
-
     @GetMapping
-    public ResponseEntity<Page<UserResponseDTO>> findAll(Pageable pageable) {
-        Page<UserResponseDTO> users = userService.findAll(pageable);
-        return ResponseEntity.ok(users);
+    public ResponseEntity<PagedModel<EntityModel<UserResponseDTO>>> findAll(Pageable pageable) {
+        Page<UserResponseDTO> page = userService.findAll(pageable);
+        return ResponseEntity.ok(pagedAsembler.toModel(page, userAssembler));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<UserResponseDTO>> findById(@PathVariable Long id) {
         var user = userService.findById(id);
+        return ResponseEntity.ok(userAssembler.toModel(user));
 
-        EntityModel<UserResponseDTO> model = EntityModel.of(user);
-
-        model.add(linkTo(methodOn(UserController.class).findById(id)).withSelfRel());
-        model.add(linkTo(methodOn(UserController.class).findAll(null)).withRel("allUsers"));
-        model.add(linkTo(methodOn(UserController.class).updateUser(id, null)).withRel("update").withType("PUT"));
-        model.add(linkTo(methodOn(UserController.class).deleteUser(id)).withRel("delete").withType("DELETE"));
-
-        return ResponseEntity.ok(model);
     }
 
     @GetMapping("username/{username}")
--- src/main/java/br/com/blog/api/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/blog/api/exception/handler/GlobalExceptionHandler.java
@@ -10,14 +10,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, WebRequest request) {

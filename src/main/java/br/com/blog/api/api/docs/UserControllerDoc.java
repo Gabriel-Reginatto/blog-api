330e0a5df938242149a9a6d312bd03211b494288
Gabriel Reginatto <reginattogb@gmail.com>
2026-04-23 16:52:49 -0300

    docs: add custom OpenAPi annotations for HTTP responses

--- src/main/java/br/com/blog/api/api/docs/UserControllerDoc.java
+++ src/main/java/br/com/blog/api/api/docs/UserControllerDoc.java
@@ -19,80 +19,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "Users", description = "Endpoints for user management")
 public interface UserControllerDoc {
 
-    @Operation(
-            summary = "Find all users",
-            description = "Returns paginated list of all users with HATEOAS links",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success"),
-                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
-            }
-    )
     ResponseEntity<PagedModel<EntityModel<UserResponseDTO>>> findAll(Pageable pageable);
 
-    @Operation(
-            summary = "Find user by their ID",
-            description = "Returns a user by ID",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal Server error", content = @Content)
-            }
-
-    )
     ResponseEntity<EntityModel<UserResponseDTO>> findById(@PathVariable Long id);
 
-
-    @SuppressWarnings("unused")
-    @Operation(
-            summary = "Find user by your username",
-            description = "Returns a user by username",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
-                    @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
-            }
-    )
     ResponseEntity<EntityModel<UserResponseDTO>> findByUsername(@PathVariable String username);
 
-    @Operation(
-            summary = "Create a user",
-            description = "Returns the created user",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
-                    @ApiResponse(responseCode = "409", description = "Username or email already exists", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal Server error", content = @Content),
-            }
-    )
     ResponseEntity<EntityModel<UserResponseDTO>> createUser(@Valid @RequestBody UserCreateRequestDTO request);
 
-    @Operation(
-            summary = "Updates a user",
-            description = "Return a updated user",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
-            }
-    )
     ResponseEntity<EntityModel<UserResponseDTO>> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequestDTO request);
 
-    @Operation(
-            summary = "Deletes a user by ID",
-            description = "Deletes a user by ID (returns no content)",
-            tags = {"Users"},
-            responses = {
-                    @ApiResponse(responseCode = "204", description = "No content", content = @Content),
-                    @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
-            }
-    )
     ResponseEntity<Void> deleteUser(@PathVariable Long id);
 }
